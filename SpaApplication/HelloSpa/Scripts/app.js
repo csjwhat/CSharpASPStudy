cd1f75a9a5f6804dd98eb397be2c01035d7a6754
csjwhats <csjwhats@gmail.com>
2020-02-26 23:36:12 +0900

    再学習

--- SpaApplication/HelloSpa/Scripts/app.js
+++ SpaApplication/HelloSpa/Scripts/app.js
@@ -1,5 +1,11 @@
+//$().ready(() => {
+//    var text = "SAMPLE";
+//    $("div").append(text);
+//})
 $().ready(function () {
-    var text = "SAMPLE";
-    $("div").append(text);
+    $.getJSON("api/Hello/Hello/", null, function (data, textStatus, jqXHR) {
+        var text = data;
+        $("div").append("<span>" + text + "</span>");
+    });
 });
 //# sourceMappingURL=app.js.map
--- SpaApplication/HelloSpa/Scripts/app.ts
+++ SpaApplication/HelloSpa/Scripts/app.ts
@@ -1,4 +1,14 @@
-﻿$().ready(() => {
-    var text = "SAMPLE";
-    $("div").append(text);
-})
+﻿//$().ready(() => {
+//    var text = "SAMPLE";
+//    $("div").append(text);
+//})
+
+$().ready(() => {
+    $.getJSON(
+        "api/Hello/Hello/",
+        null,
+        (data, textStatus, jqXHR) => {
+            var text = data;
+            $("div").append("<span>" + text + "</span>");
+        })
+});
